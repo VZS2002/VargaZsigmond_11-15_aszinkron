63505de1d68ef5892d3876ca0db95efedbac60f5
Varga Zsigmond <vargazsigmond02@gmail.com>
2022-11-15 10:57:55 +0100

    Darabszám feladat

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './style.css';
 
 let lista;
+let hossz;
+
+
 
 document.addEventListener('DOMContentLoaded', () => {
     
@@ -16,12 +19,15 @@ document.addEventListener('DOMContentLoaded', () => {
         
     }
    
+
+    
    
     function kiir(idezetek) {
         let idezetLista = document.getElementById('idezetek');
      
         for (let p of idezetek) {
             let li = document.createElement('li');
+            
             li.textContent =" '' " +p.quote+"'' -"+p.author;
             idezetLista.appendChild(li);
         }
@@ -30,8 +36,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     document.getElementById('idezet').addEventListener('click', () => {
-        
+        osszadatBetoltes();
         kiir(lista);
+     
     });
 
+    document.getElementById('the').addEventListener('click', () => {
+        osszadatBetoltes();
+      
+    
+    
+     
+    });
+
+    document.getElementById('hossz').addEventListener('click', () => {
+        osszadatBetoltes();
+        let hosszLista = document.getElementById('szoveg');
+    for (let i = 0; i < lista.length; i++) {
+       let p = document.createElement('p');
+       p.textContent= lista[i].quote.length;
+       hosszLista.appendChild(p);
+    }
+    
+     
+    });
+
+    document.getElementById('darabszam').addEventListener('click', () => {
+        osszadatBetoltes();
+        document.getElementById('darab').value=0;
+      
+        for (let i = 0; i < lista.length; i++) {
+           if (lista[i].author== document.getElementById('szerzo').value) {
+            
+            document.getElementById('darab').value++;
+           }
+            
+        }
+    
+    
+     
+    });
 })
+
