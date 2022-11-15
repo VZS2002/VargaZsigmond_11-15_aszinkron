5b6b8b8599298283afdaac097054d17baad15d34
Varga Zsigmond <vargazsigmond02@gmail.com>
2022-11-15 09:10:58 +0100

    Elso fealadat

--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
 import './style.css';
 
+let lista;
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+
+
+    async function osszadatBetoltes() {
+      
+        
+        let response = await fetch('/quotes.json');
+        let eredmeny = await response.json();
+        lista = eredmeny.quotes.sort((a, b) => a.author.localeCompare(b.author));
+
+        
+    }
+   
+   
+    function kiir(idezetek) {
+        let idezetLista = document.getElementById('idezetek');
+     
+        for (let p of idezetek) {
+            let li = document.createElement('li');
+            li.textContent =" '' " +p.quote+"'' -"+p.author;
+            idezetLista.appendChild(li);
+        }
+    }
+    osszadatBetoltes();
+
+
+    document.getElementById('idezet').addEventListener('click', () => {
+        
+        kiir(lista);
+    });
+
+})
